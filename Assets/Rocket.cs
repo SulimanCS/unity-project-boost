1dea3d92c644d18ef7ee4f438df5756da0d39103
SulimanCS <sulimanAlsarraf@gmail.com>
2020-07-02 21:30:26 -0700

    Delay loading a level when dying or transcending

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -9,6 +9,9 @@ public class Rocket : MonoBehaviour {
   Rigidbody rigitBody;
   AudioSource audioSource;
 
+  enum State { Alive, Dying, Transcending }
+  State state = State.Alive;
+
   // Start is called before the first frame update
   void Start() {
     rigitBody = GetComponent<Rigidbody>();
@@ -28,43 +31,47 @@ public class Rocket : MonoBehaviour {
         break;
       case "Finish":
         print("NEXT LEVEL");
-        SceneManager.LoadScene(1);
+        state = State.Transcending;
+        Invoke("LoadNextLevel", 0.5f);
         break;
       default:
         print("YOU MESSED UP");
-        SceneManager.LoadScene(0);
+        state = State.Dying;
+        Invoke("LoadFirstLevel", 1f);
         // kill player
         break;
     }
   }
 
-  private void Rotate()
-  {
+  private void LoadFirstLevel() {
+    SceneManager.LoadScene(0);
+  }
+
+  private void LoadNextLevel() {
+    SceneManager.LoadScene(1);
+  }
+
+  private void Rotate() {
     rigitBody.freezeRotation = true; // take manual control of rotation
 
     float rotationThisFrame = rcsThrust * Time.deltaTime;
 
-    if (Input.GetKey(KeyCode.A))
-    {
+    if (Input.GetKey(KeyCode.A)) {
       transform.Rotate(Vector3.forward * rotationThisFrame);
     }
-    else if (Input.GetKey(KeyCode.D))
-    {
+    else if (Input.GetKey(KeyCode.D)) {
       transform.Rotate(-Vector3.forward * rotationThisFrame);
     }
 
     rigitBody.freezeRotation = false; // resume physics control of rotation
   }
 
-  private void Thrust()
-  {
-    if (Input.GetKey(KeyCode.Space))
-    {
+  private void Thrust() {
+    if (Input.GetKey(KeyCode.Space)) {
       rigitBody.AddRelativeForce(Vector3.up * mainThrust);
       if (!audioSource.isPlaying) audioSource.Play();
     }
-    else
-    {
+    else {
       audioSource.Stop();
     }
   }
