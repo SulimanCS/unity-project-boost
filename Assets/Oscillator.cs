c1877339eedab1b25b7ba0400dcaa1edbfdf2172
SulimanCS <sulimanAlsarraf@gmail.com>
2020-07-03 22:52:28 -0700

    Enable obstacle oscillation from unity's inspector

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+  // TODO: remove from inspector later
+  [SerializeField] Vector3 movementVector;
+
+  [SerializeField] [Range(0, 1)] float movementFactor;
+
+  Vector3 startingPos;
+  // Start is called before the first frame update
+  void Start() {
+    startingPos = transform.position;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Vector3 offset = movementVector * movementFactor;
+    transform.position = startingPos + offset;
+  }
+}
