2f9c276bbe4dd6c718d7b4f42095c9f70ddf1e59
SulimanCS <sulimanAlsarraf@gmail.com>
2020-07-04 22:27:49 -0700

    Enable complete oscillation cycles from code

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour {
 
   // TODO: remove from inspector later
-  [SerializeField] Vector3 movementVector;
+  [SerializeField] Vector3 movementVector = new Vector3 (10f, 10f, 10f);
+  [SerializeField] float period = 2f;
 
   [SerializeField] [Range(0, 1)] float movementFactor;
 
@@ -18,6 +19,10 @@ public class Oscillator : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
+    float cycles = Time.time / period;
+    const float tau = Mathf.PI * 2f; // about 6.28
+    float rawSinWave = Mathf.Sin(cycles * tau);
+    movementFactor = (rawSinWave / 2f) + 0.5f;
     Vector3 offset = movementVector * movementFactor;
     transform.position = startingPos + offset;
   }
