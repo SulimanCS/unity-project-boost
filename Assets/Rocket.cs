374d8e3237558f28faad0064d31dd3e07d41ca2a
SulimanCS <sulimanAlsarraf@gmail.com>
2020-07-04 21:33:13 -0700

    Fix a bug with thurst, make it frame rate independent

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -102,9 +102,8 @@ public class Rocket : MonoBehaviour {
     }
   }
 
-  private void ApplyThrust()
-  {
-    rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+  private void ApplyThrust() {
+    rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
     if (!audioSource.isPlaying) audioSource.PlayOneShot(mainEngine);
     if (!mainEngineParticles.isPlaying) mainEngineParticles.Play();
   }
