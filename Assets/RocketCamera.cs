c51c7460e0dde78a00f9e2e45e2e6f010260522f
SulimanCS <sulimanAlsarraf@gmail.com>
2020-07-18 08:43:15 -0700

    Add a script to follow the rocket across a given level horizontally

---
+++ Assets/RocketCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketCamera : MonoBehaviour {
+
+  GameObject rocket;
+  float originalZ = 0;
+
+  // Start is called before the first frame update
+  void Start() {
+    rocket = GameObject.Find("Rocket Ship");
+    originalZ = transform.position.z;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    float rocketPositionX = rocket.transform.position.x;
+    float rocketPositionY = rocket.transform.position.y;
+    transform.position = new Vector3(rocketPositionX, rocketPositionY, originalZ);
+  }
+}
